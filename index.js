6f5fc1f1fef9dbfa21a57a9d298d1a043fb02689
kamrulemon0 <kamrulemon009911@gmail.com>
2022-10-21 10:02:04 +0600

    mcq-1

---
+++ index.js
@@ -0,0 +1,23 @@
+function count(...basket) {
+    // console.log(basket.length)
+  }
+  
+  count(10, 9, 8, 7, 6);
+  
+  
+  const workoutJournal = (miles, avgTime) => {
+    // console.log('I ran ' + miles + ' miles at an average of ' + avgTime + ' per mile.');
+  };
+   
+  workoutJournal('3');
+  
+  
+  // function volumeOfCube (side) {
+  //   return side * side * side;
+  // }
+   
+  // volumeOfCube(5);
+  
+  const multiplier = (number) => {
+    console.log(3 * number);
+  };
